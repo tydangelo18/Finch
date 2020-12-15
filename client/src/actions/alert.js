3dde36de1f8e4867bec91b8319172df2fdc42a5b
Ty D'Angelo <tydangelo18@gmail.com>
2020-12-15 14:14:49 -0600

    Implemented setAlert action in DeleteButton component.

--- client/src/actions/alert.js
+++ client/src/actions/alert.js
@@ -2,7 +2,7 @@ import { v4 as uuid } from 'uuid';
 import { SET_ALERT, REMOVE_ALERT } from './types';
 
 // Action to dispatch the type to the reducer (adds alert to the state)
-export const setAlert = (msg, alertType, timeout = 5000) => (dispatch) => {
+export const setAlert = (msg, alertType, timeout = 4000) => (dispatch) => {
   const id = uuid();
   dispatch({
     type: SET_ALERT,
--- client/src/components/Saved/DeleteButton.js
+++ client/src/components/Saved/DeleteButton.js
@@ -1,13 +1,29 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { setAlert } from '../../actions/alert';
 import DeleteForeverOutlinedIcon from '@material-ui/icons/DeleteForeverOutlined';
 
-export default function DeleteButton(props) {
-  const { handleDelete, eventId } = props;
+const DeleteButton = (props) => {
+  // Destructure handleClick and setAlert so that we can pass them in as props in the onClick function
+  const { handleDelete, eventId, setAlert } = props;
   return (
     <div className='deleteBtn'>
-      <button className='btn' onClick={() => handleDelete(eventId)}>
+      <button
+        className='btn'
+        onClick={() => {
+          handleDelete(eventId);
+          setAlert('Tweet Deleted! ', 'success');
+        }}
+      >
         <DeleteForeverOutlinedIcon />
       </button>
     </div>
   );
-}
+};
+
+DeleteButton.propTypes = {
+  setAlert: PropTypes.func.isRequired,
+};
+
+export default connect(null, { setAlert })(DeleteButton);
