c01ee2e01929e80bd59a0d0cd70758a51af16626
Ty D'Angelo <tydangelo18@gmail.com>
2020-12-16 13:14:26 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 // Require Dependencies
 const express = require('express');
-const connectDB = require('./db/Connection');
+//const connectDB = require('./db/Connection');
 const path = require('path');
 const bodyParser = require('body-parser');
 const passport = require('passport');
@@ -10,11 +10,25 @@ const tweets = require('./routes/api/tweets');
 const axios = require('axios');
 require('dotenv').config();
 
+const mongoose = require('mongoose');
+
+const URI =
+  'mongodb+srv://ty18:pQUsHphiwU5Qg5HD@finch.ccdd9.mongodb.net/finch?retryWrites=true&w=majority';
+const connectionDB = async () => {
+  await mongoose.connect(URI, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  });
+  console.log('DB has been connected!');
+};
+
+//module.exports = connectionDB;
+
 // Initialize Express for app
 const app = express();
 
-connectDB();
-
+//connectDB();
+connectionDB();
 
 // Bodyparser Middleware
 app.use(
